554da9375031523ae99c3eea0eb15b8b4a22df49
K-CHI-94 <kjwsq94@naver.com>
2024-06-20 17:24:22 +0900

    git push -u origin develop test3

--- buddy_react/src/App.jsx
+++ buddy_react/src/App.jsx
@@ -25,6 +25,7 @@ function App() {
         <button>feature calendar test2</button>
         <button>git push -u origin develop test</button>
         <button>git push -u origin develop test2</button>
+        <button>git push -u origin develop test3</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
